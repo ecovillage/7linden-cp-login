a79a5b5139949f6791f7246fa06aa1119598c4d3
Felix Wolfsteller <felix.wolfsteller@gmail.com>
2015-12-05 22:28:24 +0100

    initial version with half working script

---
+++ create.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+# Copyright 2015 Felix Wolfsteller
+# Released under the GPL
+
+# Create 7linden-cp-login**.deb
+
+fpm -s dir\
+    -t deb\
+    -v `cat VERSION`\
+    -a all\
+    --name "7linden-cp-login"\
+    --description "Manually login to captive portal"\
+    --directories opt/7linden-cp-login\
+    --depends bash\
+    --depends wget\
+    --depends whiptail\
+    --depends openssh-client\
+    --depends diffutils\
+    --license "GPLv3"\
+    --maintainer "felix.wolfsteller@gmail.com"\
+    etc opt
+# --after-install --after-remove
